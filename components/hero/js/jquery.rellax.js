9ec643aeed2718e7bb9b612611ea4b03a39532ce
Vlad Olaru <vlad.olaru@pixelgrade.com>
2020-03-31 16:18:41 +0300

    Pixelgrade brand consistency fixes

--- components/hero/js/jquery.rellax.js
+++ components/hero/js/jquery.rellax.js
@@ -1,7 +1,7 @@
 /*!
  * jQuery Rellax Plugin v0.3.6.4
  * Examples and documentation at http://pixelgrade.github.io/rellax/
- * Copyright (c) 2016 PixelGrade http://www.pixelgrade.com
+ * Copyright (c) 2016 Pixelgrade https://pixelgrade.com
  * Licensed under MIT http://www.opensource.org/licenses/mit-license.php/
  */
 ;(
